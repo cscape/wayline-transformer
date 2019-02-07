0dec90b91bbb1fcec123f5ea64cdbe7cfdc9ba6e
ciolt <cioltdev@outlook.com>
2019-02-06 23:16:49 -0500

    add TSO PubTrans News cleaner

---
+++ api/clean/TSOMobile/PubTransNews.js
@@ -0,0 +1,16 @@
+const cleanProps = require('../../casing/tso')
+
+/**
+ * Cleans the PubTrans/GetModuleInfoPublic?Key=NEWS response
+ * @param {{ID: string, Type: string, News: string}[]} jsonArr JSON array after converting the parsed response
+ * @returns {{id: string, type: number, news: string}[]} An array of news objects
+ */
+const PubTransRoutes = (jsonArr) => {
+  const arr = jsonArr
+    .map(o => ({ ...o, Type: Number(o.Type) }))
+    .map(o => cleanProps.object(o))
+
+  return arr
+}
+
+module.exports = PubTransRoutes
