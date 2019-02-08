2774abaf38a0e3cc505e9b5359ef9cff2c142b3f
ciolt <cioltdev@outlook.com>
2019-02-08 18:30:46 -0500

    fix typo (embarrassing)

--- api/clean/TSOMobile/PubTransNews.js
+++ api/clean/TSOMobile/PubTransNews.js
@@ -5,7 +5,7 @@ const cleanProps = require('../../casing/tso')
  * @param {{ID: string, Type: string, News: string}[]} jsonArr JSON array after converting the parsed response
  * @returns {{id: string, type: number, news: string}[]} An array of news objects
  */
-const PubTransRoutes = (jsonArr) => {
+const PubTransNews = (jsonArr) => {
   const arr = jsonArr
     .map(o => ({ ...o, Type: Number(o.Type) }))
     .map(o => cleanProps.object(o))
@@ -13,4 +13,4 @@ const PubTransRoutes = (jsonArr) => {
   return arr
 }
 
-module.exports = PubTransRoutes
+module.exports = PubTransNews
