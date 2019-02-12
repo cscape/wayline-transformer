01719b04f2427c938644bc3f7f3c2e4bdab4344b
ciolt <cioltdev@outlook.com>
2019-02-12 13:37:29 -0500

    add TSO Mobile PubTrans ROUTE_STOPS_AND_UNITS cleaner

---
+++ api/clean/TSOMobile/PubTransRouteStops.js
@@ -0,0 +1,17 @@
+const cleanProps = require('../../casing/tso')
+
+/**
+ * Cleans the PubTrans/GetModuleInfoPublic?Key=ROUTE_STOPS_AND_UNITS response
+ * @param {{}[]} jsonArr JSON array after converting the parsed response
+ * @returns {{stop_id: string, stop_number: string, lat: string, long: string}[]} An array of stops
+ */
+const PubTransRouteStopsUnits = (jsonArr) => {
+  const arr = jsonArr[0] // arr[1] contains realtime bus data, not needed
+
+    .map(i => { delete i.Icon; i.stop_id = i.ID; delete i.ID; return i })
+    .map(i => cleanProps.object(i))
+
+  return arr
+}
+
+module.exports = PubTransRouteStopsUnits
