7385fcdc148f654fad6129e698cdaf8ade610d63
ciolt <cioltdev@outlook.com>
2019-02-12 14:02:34 -0500

    Add PubTrans UNIT LOCATION cleaner, updates #1

---
+++ api/clean/TSOMobile/PubTransLocations.js
@@ -0,0 +1,27 @@
+const cleanProps = require('../../casing/tso')
+
+/**
+ * Cleans the PubTrans/GetModuleInfoPublic?Key=UNITS_LOCATION_ROUTE response
+ * @param {{}[]} jsonArr JSON array after converting the parsed response
+ * @returns {{}[]} An array of locations /bus objects
+ */
+const PubTransRoutes = (jsonArr) => {
+  let arr = jsonArr
+    .map(o => ({
+      ...o,
+      timestamp: Number(o.Tim) * 1000,
+      name: o.ShortName
+    }))
+    .map(o => {
+      delete o.AntibunchingCmd; delete o.Occupation
+      delete o.RealStopID; delete o.SquareID
+      delete o.StopA; delete o.StopB
+      delete o.Tim; delete o.ShortName
+      return o
+    })
+    .map(o => cleanProps.object(o))
+
+  return arr
+}
+
+module.exports = PubTransRoutes
