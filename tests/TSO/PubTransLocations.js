8db86184bbbf402f0376349fd6689f1b42cec87d
ciolt <cioltdev@outlook.com>
2019-02-12 14:03:05 -0500

    add TSO PubTrans Locations test

---
+++ tests/TSO/PubTransLocations.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+const cleaner = require('../../api/clean/TSOMobile/PubTransLocations')
+const WaylineConfig = require('@wayline/config')
+
+const TransitXMLFeed = WaylineConfig.basefeeds.TSOMobile
+
+axios.get(`${TransitXMLFeed}PubTrans/GetModuleInfoPublic`, {
+  params: {
+    Key: 'UNITS_LOCATION_ROUTE',
+    id: 92089,
+    lan: 'en'
+  }
+}).then(function (response) {
+  const jsonData = JSON.parse(response.data)
+  const routes = cleaner(jsonData)
+
+  console.log(routes)
+})
