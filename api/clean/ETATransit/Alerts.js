7eec1ea03630cc331137b246c4ba354151be8f40
ciolt <cioltdev@outlook.com>
2019-02-24 01:18:41 -0500

    update ETA transit alerts cleaner

--- api/clean/ETATransit/Alerts.js
+++ api/clean/ETATransit/Alerts.js
@@ -5,15 +5,18 @@ const cleanProps = require('../../casing/eta-transit')
  * @param {{get_vehicles: []}} jsonObj ETA Transit response JSON object
  * @returns {{}[]} An array of service alert objects
  */
-const FormatAlerts = (jsonObj) => {
+const FormatAlerts = jsonObj => {
   const obj = jsonObj
     .get_service_announcements
-    .map(o => o.map(a => ({
-      ...a,
-      start: new Date(a.start).getTime(),
-      end: new Date(a.end).getTime()
-    })))
-    .map(o => cleanProps.object(o))
+    .map(o => {
+      o[cleanProps.single('announcements')] = o.announcements.map(a => ({
+        ...a,
+        start: new Date(a.start).getTime(),
+        end: new Date(a.end).getTime()
+      }))
+      delete o.announcements
+      return o
+    })
 
   return obj
 }
