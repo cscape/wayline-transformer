15d3d2126b15868bc79218681ffcbaa7cef06eb3
ciolt <cioltdev@outlook.com>
2019-02-24 00:48:02 -0500

    Update cleaner for ETA Transit

--- api/clean/ETATransit/Vehicles.js
+++ api/clean/ETATransit/Vehicles.js
@@ -1,12 +1,26 @@
 const cleanProps = require('../../casing/eta-transit')
 
+const removeProps = [
+  'tripID', 'load', 'capacity', 'blockID', 'nextStopID',
+  'nextStopETA', 'nextStopETA', 'nextPatternStopID',
+  'minutesToNextStops', 'deadHead', 'inService', 'aID'
+]
+
 /**
  * Cleans the service.get_vehicles response
  * @param {{get_vehicles: []}} jsonObj ETA Transit response JSON object
- * @returns {{}[]} An array of route objects
+ * @returns {{}[]} An array of vehicle objects
  */
 const FormatVehicles = (jsonObj) => {
   const obj = jsonObj
+    .get_vehicles
+    .map(o => {
+      o.running = Boolean(o.inService)
+      removeProps.forEach(a => delete o[a])
+      return o
+    })
+    .map(o => cleanProps.object(o))
+
   return obj
 }
 
