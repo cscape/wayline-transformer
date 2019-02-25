a3efe22f2335fb239a181682a39e25bb3f3350d2
ciolt <cioltdev@outlook.com>
2019-02-25 11:51:22 -0500

    Add cleaner for GIS bus routes

---
+++ api/clean/MiamiDadeGIS/BusRoutes.js
@@ -0,0 +1,23 @@
+const cleanProps = require('../../casing/eta-transit')
+const flatmap = require('../../util/flatmap')
+const encode = require('../../util/polyline')
+
+/**
+ * Cleans the BusRoutes_gdb response from the MDT-GIS FeatureServer
+ * @param {{}} jsonObj FeatureServer response
+ * @returns {{}[]} An array of routes
+ */
+const FormatObj = jsonObj => {
+  const obj = jsonObj
+    .features
+    .map(o => {
+      const attr = cleanProps.object(o.attributes)
+      const coordsFlatmap = flatmap(o.geometry.paths)
+      const polyline = encode(coordsFlatmap)
+      return { ...attr, polyline }
+    })
+
+  return obj
+}
+
+module.exports = FormatObj
