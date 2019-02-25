8ccd4acafa18165cda4a2f5b32c8f71b2a67a980
ciolt <cioltdev@outlook.com>
2019-02-24 19:37:47 -0500

    complete the get_patterns ETA transit cleaner

---
+++ api/clean/ETATransit/Patterns.js
@@ -0,0 +1,27 @@
+const cleanProps = require('../../casing/eta-transit')
+const colors = require('../../util/colors')
+
+const removeProps = [
+  'decLine', 'extID', 'stations', 'type', 'routeNames'
+]
+
+/**
+ * Cleans the service.get_patterns response
+ * @param {{get_patterns: []}} jsonObj ETA Transit response JSON object
+ * @returns {{}[]} An array of pattern objects
+ */
+const FormatObject = jsonObj => {
+  const obj = jsonObj
+    .get_patterns
+    .map(o => {
+      o.color = colors.hexToRgb(o.color)
+      o.route_name = (o.routeNames.length > 0 ? o.routeNames[0] : '')
+      removeProps.forEach(a => delete o[a])
+      return o
+    })
+    .map(o => cleanProps.object(o))
+
+  return obj
+}
+
+module.exports = FormatObject
