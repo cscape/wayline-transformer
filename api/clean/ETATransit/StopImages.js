dbfa2edf6e609b26af7e3fc3da56085bf4a5f495
ciolt <cioltdev@outlook.com>
2019-02-24 19:53:39 -0500

    update name

--- api/clean/ETATransit/StopImages.js
+++ api/clean/ETATransit/StopImages.js
@@ -5,13 +5,13 @@ const removeProps = [
 ]
 
 /**
- * Cleans the service.get_stops response
- * @param {{get_stops: []}} jsonObj ETA Transit response JSON object
+ * Cleans the service.get_stopimages response
+ * @param {{get_stopimages: []}} jsonObj ETA Transit response JSON object
  * @returns {{}[]} An array of stop objects
  */
 const FormatObject = jsonObj => {
   const obj = jsonObj
-    .get_stops
+    .get_stopimages
     .map(o => {
       o.images = o.images.filter(a => a !== '')
       removeProps.forEach(a => delete o[a])
