87ab39558003d251af8e171e73b27b675e559e84
ciolt <cioltdev@outlook.com>
2019-02-24 19:18:37 -0500

    update colors util to filter non-hex

--- api/util/colors.js
+++ api/util/colors.js
@@ -4,7 +4,7 @@ const rgbToHex = (r, g, b) => [r, g, b].map(x => {
 }).join('')
 
 const hexToRgb = hex => {
-  hex = hex.replace(/^#/, '')
+  hex = hex.replace(/[^A-Fa-f0-9]/g, '')
 
   if (hex.length === 3) {
     hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
