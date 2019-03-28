852eee27d5af23dccfe6e4a0fbb8e31fc9717ff6
ciolt <cioltdev@outlook.com>
2019-03-28 12:41:23 -0400

    add buses casing info

---
+++ api/casing/buses.js
@@ -0,0 +1,12 @@
+module.exports = require('./_fmap')({
+  'BusName': 'name',
+  'BusID': 'id',
+  'Direction': 'direction',
+  'RouteID': 'route_id',
+  'TripID': 'trip_id',
+  'ServiceDirection': 'service_direction',
+  'TripHeadsign': 'headsign',
+  'LocationUpdated': 'timestamp',
+  'Latitude': 'lat',
+  'Longitude': 'lng'
+})
