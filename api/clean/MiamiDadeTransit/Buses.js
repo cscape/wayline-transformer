17b447cd38fcdfcca7ad05b93300b808c3d73563
ciolt <cioltdev@outlook.com>
2019-03-28 16:35:45 -0400

    Add buses cleaner API

---
+++ api/clean/MiamiDadeTransit/Buses.js
@@ -0,0 +1,34 @@
+const cleanProps = require('../../casing/buses')
+const { toTimestamp } = require('../../util/time-reformat')
+
+/**
+ * Cleans the Buses (realtime) response
+ * @param {{RecordSet}} jsonObj JSON object after converting the response from XML
+ * @returns {string[]} An array of buses as objects
+ */
+const FormatObject = jsonObj => {
+  const { RecordSet } = jsonObj
+  const noRecords = RecordSet.Record == null ||
+    RecordSet === '' ||
+    typeof RecordSet === 'string' ||
+    typeof RecordSet.Record !== 'object'
+
+  // No records
+  if (noRecords) return []
+
+  let buses = []
+  if (!Array.isArray(RecordSet.Record)) buses.push(RecordSet.Record)
+  else buses = RecordSet.Record
+
+  const busArray = buses.map(o => {
+    delete o.Service
+    delete o.ServiceName
+    o = cleanProps.object(o)
+    o.timestamp = toTimestamp(o.timestamp)
+    return o
+  })
+
+  return busArray
+}
+
+module.exports = FormatObject
