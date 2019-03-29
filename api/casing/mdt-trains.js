a3d6ca8fe949641ddef48c9a4420f333937b8c95
ciolt <cioltdev@outlook.com>
2019-03-28 23:29:00 -0400

    update mdt-trains casing api

--- api/casing/mdt-trains.js
+++ api/casing/mdt-trains.js
@@ -1,11 +1,9 @@
 module.exports = require('./_fmap')({
-  'BusName': 'name',
-  'BusID': 'id',
+  'LineID': 'route',
+  'TrainID': 'id',
   'Direction': 'direction',
-  'RouteID': 'route_id',
   'TripID': 'trip_id',
   'ServiceDirection': 'service_direction',
-  'TripHeadsign': 'headsign',
   'LocationUpdated': 'timestamp',
   'Latitude': 'lat',
   'Longitude': 'lng'
