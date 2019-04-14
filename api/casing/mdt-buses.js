8a0b4c9add5a401cb73b98963275a793c60995d7
ciolt <cioltdev@outlook.com>
2019-04-14 16:08:08 -0400

    add more notes to MDT buses casing api

--- api/casing/mdt-buses.js
+++ api/casing/mdt-buses.js
@@ -1,7 +1,8 @@
 module.exports = require('./_fmap')({
   'BusName': 'name', // STRING that identifies bus "serial number", see [1]
   'BusID': 'id', // unique ID for this bus
-  'Direction': 'direction',
+  'Direction': 'direction', // Only these: N, NE, E, SE, S, SW, W, NW
+  'vehDirection': 'direction',
   'RouteID': 'route_id',
   'TripID': 'trip_id',
   'ServiceDirection': 'service_direction',
@@ -9,13 +10,12 @@ module.exports = require('./_fmap')({
   'LocationUpdated': 'timestamp',
   'Latitude': 'lat',
   'Longitude': 'lng',
-  'vehDirection': 'direction',
-  'OnTime': 'delay',
-  'LocationUpdatedDiffBasic': 'timestamp_offset',
+  'OnTime': 'delay', // pos = arriving late, neg(-) = ahead of schedule, arriving early
+  'LocationUpdatedDiffBasic': 'timestamp_offset', // in seconds
   'vehSpeed': 'speed',
   'ShapeID': 'shape_id',
   'Route': 'route_id',
-  'RouteAlias': 'name'
+  'RouteAlias': 'route_name'
 })
 
 /* [1] Each bus has a serial ID, which represents when the bus
