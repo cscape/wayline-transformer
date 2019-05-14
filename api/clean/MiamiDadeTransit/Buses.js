35a407b44886cb3d64c533ad0d1bae81852ae27d
ciolt <cioltdev@outlook.com>
2019-05-13 22:16:22 -0400

    Using Buses/Trains.js will throw an error

--- api/clean/MiamiDadeTransit/Buses.js
+++ api/clean/MiamiDadeTransit/Buses.js
@@ -1,38 +1,10 @@
-const cleanProps = require('../../casing/mdt-buses')
-const { toTimestamp } = require('../../util/time-reformat')
-const toBearing = require('../../util/convert-direction')
-
 /**
  * Cleans the Buses (realtime) response
  * @param {{RecordSet}} jsonObj JSON object after converting the response from XML
  * @returns {string[]} An array of buses as objects
  */
 const FormatObject = jsonObj => {
-  const { RecordSet } = jsonObj
-  const noRecords = RecordSet.Record == null ||
-    RecordSet === '' ||
-    typeof RecordSet === 'string' ||
-    typeof RecordSet.Record !== 'object'
-
-  // No records
-  if (noRecords) return []
-
-  let buses = []
-  if (!Array.isArray(RecordSet.Record)) buses.push(RecordSet.Record)
-  else buses = RecordSet.Record
-
-  const busArray = buses.map(o => {
-    delete o.Service
-    delete o.ServiceName
-    let a = o.bearing
-    o.bearing = toBearing(a)
-    o = cleanProps.object(o)
-    o.name = String(o.name)
-    o.timestamp = toTimestamp(o.timestamp)
-    return o
-  })
-
-  return busArray
+  throw new Error('Please use MiamiDadeTransit/BusesVerbose')
 }
 
 module.exports = FormatObject
