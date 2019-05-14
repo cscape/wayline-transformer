1dead78b652861614b6ef0ff3f6b5aac1c0d3f04
ciolt <cioltdev@outlook.com>
2019-05-14 11:59:43 -0400

    add toLong utility

--- api/util/make-timestamp.js
+++ api/util/make-timestamp.js
@@ -1,3 +1,5 @@
+const Long = require('long')
+
 /**
  * Convert JavaScript timestamp to 10 digits, and return string
  * @param {number} dateObj A unix timestamp, can be generated with `Date.now()`
@@ -8,6 +10,13 @@ const fromTime = (dateObj = Date.now()) => {
     .substring(0, 10) // MBTA has a 10-character timestamp
 }
 
+const toLong = (dateString) => {
+  if (typeof dateString !== 'string') dateString = fromTime(dateString)
+  // The MBTA uses an unsigned Long
+  const timestamp = new Long(dateString, Long.UZERO, true)
+  return timestamp
+}
+
 module.exports = {
-  fromTime
+  fromTime, toLong
 }
