0019cc261de526e53a0bae0292d5ad2e0ea7feef
ciolt <cioltdev@outlook.com>
2019-05-14 21:09:44 -0400

    Add MoverTrains realtime data

---
+++ api/casing/mdt-movertrains.js
@@ -0,0 +1,12 @@
+module.exports = require('./_fmap')({
+  'Direction': 'direction',
+  'LoopID': 'loop_id',
+  'LoopName': 'loop_name',
+  'TrainID': 'id',
+  'Latitude': 'lat',
+  'Longitude': 'lng',
+  'Cars': 'cars',
+  'vehDirection': 'bearing',
+  'vehSpeed': 'speed',
+  'LocationUpdatedDiffBasic': 'timestamp_offset' // in seconds
+})
---
+++ api/clean/MiamiDadeTransit/MoverTrainsVerbose.js
@@ -0,0 +1,31 @@
+const cleanProps = require('../../casing/mdt-movers')
+const { elapsedNow } = require('../../util/time-reformat')
+const toBearing = require('../../util/convert-direction')
+const cleanifyJSON = require('../../util/mdt-clean-response')
+
+/**
+ * VERBOSE - Cleans the MoverTrains (realtime) response
+ * @param {{MoverTrains}} jsonObj JSON object after converting the response from XML
+ * @returns {string[]} An array of MoverTrains as objects
+ */
+const FormatObject = jsonObj => {
+  const movers = cleanifyJSON('MoverTrains', 'Train', jsonObj)
+
+  const p2Delete = [
+    'Color', 'ServiceDirection', 'Service', 'LocationUpdatedDiff',
+    'LocationUpdated', 'LoopName'
+  ]
+
+  const moversArray = movers.map(o => {
+    p2Delete.forEach(i => delete o[i])
+    o = cleanProps.object(o)
+    o.bearing = toBearing(o.bearing)
+    o.timestamp = elapsedNow(o.timestamp_offset)
+    o.cars = o.cars.split('-').map(a => Number(a))
+    return o
+  })
+
+  return moversArray
+}
+
+module.exports = FormatObject
